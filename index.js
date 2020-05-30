14d39c1d9181a701f8797350ac34a9147aa28670
abhiJit1993 <abhijitsom1993@gmail.com>
2020-05-30 19:46:48 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-vbnvnv
+class Person {
+constructor (name,age=0) {
+  this.name =  name;
+  this.age =  age;
+}
+getGreetings () { 
+  return `Hi ${this.name} !!! How are you?`
+}
+getDescription () { 
+  return `${this.name} is ${this.age}  years old !!!`
+}
+}
+
+const me  = new Person('Abhijit' , 25);
+console.log(me.getGreetings(),me.getDescription());
+const she  = new Person('Neha' );
+console.log(she.getGreetings(), she.getDescription());
