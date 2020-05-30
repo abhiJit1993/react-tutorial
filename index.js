d054f11db9fb7e4b254184e9692f12327a2b740f
abhiJit1993 <abhijitsom1993@gmail.com>
2020-05-30 20:06:48 +0530

    basic react component and its rendering

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-console.log('Starting components')
+ import React, { Component } from 'react';
+ import ReactDom , { Component } from 'react-dom';
+
+class Header extends React.Component {
+  render ()  {
+    return  (
+      <div>
+      <h1> this is the header </h1>
+      </div>
+    )
+  }
+}
+class Action extends React.Component {
+  render () {
+    return (
+      <div>
+      <button> What should I do ?</button>
+      </div>
+   )
+  }
+}
+
+class Options extends React.Component { 
+  render () { 
+    return  (
+      <div>
+      These are the options!!!
+      </div>
+    )
+  }
+}
+
+class AddOption  extends React.Component{
+  render() {
+    return  (
+      <div> 
+    <button>Add Option </button>
+      </div>
+    )
+  }
+}
+
+
+
+
+const jsx = ( <div> 
+<Header/>
+<Action/>
+<Options/>
+<AddOption/>
+</div> 
+);
+
+
+
+ReactDom.render(jsx, document.getElementById('app'))
