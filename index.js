460f50e9c7cc53ace36c47cbb7a5466d8064fd67
abhiJit1993 <abhijitsom1993@gmail.com>
2020-05-30 19:48:09 +0530

    Components started

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-class Person {
-constructor (name,age=0) {
-  this.name =  name;
-  this.age =  age;
-}
-getGreetings () { 
-  return `Hi ${this.name} !!! How are you?`
-}
-getDescription () { 
-  return `${this.name} is ${this.age}  years old !!!`
-}
-}
-
-const me  = new Person('Abhijit' , 25);
-console.log(me.getGreetings(),me.getDescription());
-const she  = new Person('Neha' );
-console.log(she.getGreetings(), she.getDescription());
+console.log('Starting components')
